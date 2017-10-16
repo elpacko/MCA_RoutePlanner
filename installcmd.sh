659beb39eb8655525322ab7d111a52f4fb279265
Francisco Javier Rivera Lujan <elpacko@gmail.com>
2017-10-15 22:49:32 -0600

    se agrega log de comandos usados para instalar dependencias

---
+++ installcmd.sh
@@ -0,0 +1,55 @@
+    1  sudo apt-get install htop
+    2  htop
+    3  ll
+    4  mkdir VRP
+    5  cd VRP/
+    6  ll
+    7  git clone https://github.com/elpacko/MCA_RoutePlanner.git
+    8  ll
+    9  htop
+   10  sudo apt-get install htop
+   11  ll
+   12  cd MCA_RoutePlanner/
+   13  ll
+   14  sudo apt-get install python
+   15  ./installRequirements.sh 
+   16  pip
+   17  sudo apt-get install pip
+   18  locate pip
+   19  locate python
+   20  locate python | grep scripts
+   21  cd /usr/lib/python3.5/venv/scripts
+   22  ll
+   23  apt install python-pip
+   24  sudo apt-get install python-pip
+   25  ll
+   26  cd
+   27  ll
+   28  cd VRP/
+   29  ll
+   30  cd MCA_RoutePlanner/
+   31  l
+   32  ll
+   33  ./installRequirements.sh 
+   34  ll
+   35  python softComputing.py 
+   36  cat installRequirements.sh 
+   37  pip install numpy
+   38  pip install --upgrade pip
+   39  python softComputing.py 
+   40  pip install matplotlib
+   41  sudo pip install matplotlib
+   42  python softComputing.py 
+   43  sudo apt-get install python-tk
+   44  python softComputing.py 
+   45  sudo pip install networkx
+   46  python softComputing.py 
+   47  sudo pip install scipy
+   48  python softComputing.py 
+   49  sudo pip install Pillow==2.6.0
+   50  python softComputing.py 
+   51  vim softComputing.py 
+   52  python softComputing.py 
+   53  vim softComputing.py 
+   54  python softComputing.py 
+   55  history
